acede015b97478197a789fc151c8ec1bb7b2cd6f
maouanaa <sofienmw@gmail.com>
2023-03-06 18:38:52 +0100

    javascriptpracticeassignement

---
+++ Predict Destructuring.js
@@ -0,0 +1,60 @@
+// problem num 1 :
+// const cars = ['Tesla', 'Mercedes', 'Honda']
+// const [ randomCar ] = cars
+// const [ ,otherRandomCar ] = cars
+// console.log(randomCar) / tesla
+// console.log(otherRandomCar) / mercedes 
+
+
+// problem num 2 :
+// const employee = {
+//     name: 'Elon',
+//     age: 47,
+//     company: 'Tesla'
+// }
+// const { name: otherName } = employee;
+
+// console.log(name); / not defined
+// console.log(otherName); / Elon
+
+
+
+// problem num 3 
+// const person = {
+//     name: 'Phil Smith',
+//     age: 47,
+//     height: '6 feet'
+// }
+// const password = '12345';
+// const { password: hashedPassword } = person;  
+// //Predict the output
+// console.log(password); / 12345
+// console.log(hashedPassword); / undefined
+
+
+
+
+// problem num 4 
+// const numbers = [8, 2, 3, 5, 6, 1, 67, 12, 2];
+// const [,first] = numbers;
+// const [,,,second] = numbers;
+// const [,,,,,,,,third] = numbers;
+// //Predict the output
+// console.log(first == second); / false 
+// console.log(first == third); / true 
+
+
+
+// problem num 5 
+// const lastTest = {
+//     key: 'value',
+//     secondKey: [1, 5, 1, 8, 3, 3]
+// }
+// const { key } = lastTest;
+// const { secondKey } = lastTest;
+// const [ ,willThisWork] = secondKey;
+// //Predict the output
+// console.log(key); / value
+// console.log(secondKey); / [1, 5, 1, 8, 3, 3]
+// console.log(secondKey[0]); / 1 
+// console.log(willThisWork); / 5 
