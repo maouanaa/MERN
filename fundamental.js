e3469cd31e3b59a1fb8dbaed10d597fded0fba42
maouanaa <sofienmw@gmail.com>
2023-03-06 17:22:19 +0100

    javascriptpracticeassignement

---
+++ fundamental.js
@@ -0,0 +1,120 @@
+// example num : 1
+// console.log(hello);                                   
+// var hello = 'world'; 
+// var hello ;
+// console.log(hello); /logs undefined
+// var hello = 'world';
+
+
+
+// example num : 2
+// var needle ;
+// function test(){
+//     var needle = 'magnet';
+//     console.log(needle);
+// }
+// var needle = 'haystack';
+// test();// magnet
+// function test(){
+//     var needle = 'magnet';
+//     console.log(needle);
+// }
+
+
+
+
+// example num 3 
+// var brendan ;
+// function print(){
+//     brendan = 'only okay';
+//     console.log(brendan);
+// var brendan = 'super cool';
+// function print(){
+//     brendan = 'only okay';
+//     console.log(brendan);
+// }
+// console.log(brendan);// super cool
+
+
+
+
+
+
+// example num 4 
+// var food ;
+// function eat(){
+//     food = 'half-chicken';
+//     console.log(food);
+//     var food = 'gone';
+// }
+// var food = 'chicken';
+// console.log(food);// chicken
+// eat(); / half chicken 
+// function eat(){
+//     food = 'half-chicken';
+//     console.log(food);
+//     var food = 'gone';
+// }
+
+
+
+// example num 5
+
+// mean(); / undefined
+// console.log(food);/ undefined
+// var mean = function() {
+//     food = "chicken";
+//     console.log(food); / chicken
+//     var food = "fish";
+//     console.log(food); / chicken
+// }
+// console.log(food); / undefined
+
+
+
+// example num 6 
+// var genre ;
+// function rewind() {
+// var genre ;
+//     genre = "rock";
+//     console.log(genre);
+//     var genre = "r&b";
+//     console.log(genre);
+// }
+
+// console.log(genre); / undefined
+// var genre = "disco";
+// rewind();
+// function rewind() {
+//     genre = "rock";
+//     console.log(genre); / rock 
+//     var genre = "r&b";
+//     console.log(genre); / r&b
+// }
+// console.log(genre); / disco 
+
+
+
+
+// example num 7 
+// function learn() {
+//     var dojo ;
+//     dojo = "seattle";
+//     console.log(dojo);
+//     var dojo = "burbank";
+//     console.log(dojo);
+// }
+
+// dojo = "san jose";
+// console.log(dojo); / san jose 
+// learn();
+// function learn() {
+//     dojo = "seattle";
+//     console.log(dojo); / seattle
+//     var dojo = "burbank";
+//     console.log(dojo); /burbank
+// }
+// console.log(dojo); / san jose
+
+
+
